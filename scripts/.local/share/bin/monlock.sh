9a4e86790fe6ffef1aebcf130d97f57dce69321f
Your Name <gj984233@gmail.com>
2024-07-07 10:29:16 +0545

    remvoed code oss due to security reasons

---
+++ scripts/.local/share/bin/monlock.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+state="${XDG_STATE_HOME}/togglemonitorlock"
+booleanvalue="false"
+
+if [[ -f ${state} ]]; then
+	booleanvalue=$(cat ${state})
+fi
+
+if [[ ${booleanvalue} == "true" ]]; then
+	wlr-randr --output eDP-1 --pos 1920,0
+	echo "false" >${state}
+else
+	wlr-randr --output eDP-1 --pos 2000,2000
+	echo "true" >${state}
+fi
